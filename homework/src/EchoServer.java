b37caaacaadddb7f5ce00e13a91f8224e2cfa256
Polikovskiy <33466335+Polikovskiy@users.noreply.github.com>
2018-07-19 14:38:41 +0300

    Update EchoServer.java

--- homework/src/EchoServer.java
+++ homework/src/EchoServer.java
@@ -5,6 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+    private static final int REPLAY_DELAY = 3000;
     private int port;
     private static int DEFAULT_PORT = 8080;
     private Socket client = null;
@@ -32,27 +33,10 @@ public class EchoServer {
 
     public void start() {
         try (ServerSocket server = new ServerSocket(port)){
-            client = server.accept();
-            this.log("Connection accepted");
-            out = new DataOutputStream(client.getOutputStream());
-            this.log("DataOutputStream created");
-            in = new DataInputStream(client.getInputStream());
-            this.log("DataInputStream created");
-            isRunning = true;
+            this.acceptConnection(server);
             while (!client.isClosed()) {
-                this.log("Server reading");
-                String entry = in.readUTF();
-                this.log("client message: " + entry);
-                this.log("Server try writing to channel");    
-                if (entry.equalsIgnoreCase("quit")) {
-                    this.log("Client inizialized closed the chanel");
-                    out.writeUTF("Server reply " + entry + " :OK");
-                    out.flush();
-                    Thread.sleep(3000);
-                    break;
-                }
-                out.writeUTF("Server reply: " + entry);
-                out.flush();
+                String incommingMessage = this.readIncommingMessage();
+                this.writeReplay(incommingMessage);
             }
             stop();
 
@@ -63,7 +47,37 @@ public class EchoServer {
         }
 
     }
-
+    
+    private void acceptConnection(ServerSocket server) {
+        client = server.accept();
+        this.log("Connection accepted");
+        out = new DataOutputStream(client.getOutputStream());
+        this.log("DataOutputStream created");
+        in = new DataInputStream(client.getInputStream());
+        this.log("DataInputStream created");
+        isRunning = true;    
+    }
+    
+    private String readIncommingMessage() {
+        this.log("Server reading");
+        String entry = in.readUTF();
+        this.log("client message: " + entry);
+        //this.log("Server try writing to channel"); 
+        return entry;
+    }
+    
+    private void writeReplay(String incommingMessage) {
+        if (incommingMessage.equalsIgnoreCase("quit")) {
+                    this.log("Client inizialized closed the chanel");
+                    out.writeUTF("Server reply " + incommingMessage + " :OK");
+                    out.flush();
+                    Thread.sleep(REPLAY_DELAY);
+                    break;
+         }
+         out.writeUTF("Server reply: " + incommingMessage);
+         out.flush();
+    }    
+    
     public void stop()  {
         this.log("Client disconnected, closing channel & ");
         try {
